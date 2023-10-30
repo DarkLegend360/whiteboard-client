27ea3c86312d7bc9e2c2a17d8c8827ba96741e60
DarkLegend360 <sidhardha.goutham@gmail.com>
2023-10-30 21:54:04 +0530

    socket fix

--- src/socket.js
+++ src/socket.js
@@ -1,9 +1,7 @@
 import { io } from "socket.io-client";
 
 const isProduction = process.env.NODE_ENV === "production";
-
-export const socket = io(
-  isProduction
-    ? "https://whiteboard-server-7vme.onrender.com"
-    : "http://localhost:4000"
-);
+const url = isProduction
+  ? "https://whiteboard-server-7vme.onrender.com"
+  : "http://localhost:4000";
+export const socket = io(url);
